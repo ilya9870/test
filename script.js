192acdffe0bb91e401bd1a3ca9779febd972ef95
Ilya <freevkgolos@mail.ru>
2019-06-20 22:54:09 +0400

    Это мой первый комит

---
+++ script.js
@@ -0,0 +1,38 @@
+"use strict"
+var leftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
+console.log(second);
+var number = 5;
+var string = "Hello";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = (2);
+console.log(4/0);
+let person = {
+    name:"John",
+    age:25,
+    isMarried:false
+
+}
+//console.log(person.name);
+//console.log(person["age"]);
+//let arr = ['plum.png','plum.jpg','plum.bmp'];
+//console.log(arr[0]);
+//alert("Hello world!");
+//console.log(answer);
+//let answer = prompt("Are you here?","Yes");
+//console.log(typeof(arr));
+//console.log("string" + 5)
+let incr = 10,
+decr = 10;
+
+console.log(incr++);
+console.log(--decr);
+console.log(5%2);
+let isChecked = false,
+isClose = false;
+
+console.log(isChecked || !isClose);
